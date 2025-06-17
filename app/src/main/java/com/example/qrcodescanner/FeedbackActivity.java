6e40f162d862b2891439ed5622d1d351b00a3c63
saitarun1806 <kunanisai123@gmail.com>
2025-06-17 13:33:06 +0530

    commit3

--- app/src/main/java/com/example/qrcodescanner/FeedbackActivity.java
+++ app/src/main/java/com/example/qrcodescanner/FeedbackActivity.java
@@ -1,15 +1,20 @@
 package com.example.qrcodescanner;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -20,6 +25,11 @@ public class FeedbackActivity extends AppCompatActivity {
 
     EditText feedbackInput;
     Button submitBtn;
+    FirebaseAuth myauth;
+    DrawerLayout drawerLayout;
+    ImageView menuBtn;
+
+
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -29,6 +39,33 @@ public class FeedbackActivity extends AppCompatActivity {
 
         feedbackInput = findViewById(R.id.et_feedback);
         submitBtn = findViewById(R.id.btn_submit_feedback);
+        myauth=FirebaseAuth.getInstance();
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navView = findViewById(R.id.nav_view);
+        menuBtn = findViewById(R.id.menu_btn);
+        menuBtn.setOnClickListener(v -> {
+            drawerLayout.openDrawer(GravityCompat.START);
+        });
+
+// Set navigation actions here ONCE
+        navView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_feedback) {
+                // Already here
+            } else if (id == R.id.nav_history) {
+                startActivity(new Intent(FeedbackActivity.this, HistoryActivity.class));
+            } else if (id == R.id.nav_scan) {
+                startActivity(new Intent(FeedbackActivity.this, ScannerActivity.class));
+            } else if (id == R.id.nav_signout) {
+                myauth.signOut();
+                Toast.makeText(this, "Signed out", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(FeedbackActivity.this, LoginActivity.class));
+                finish();
+            }
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
 
         submitBtn.setOnClickListener(v -> {
             String feedback = feedbackInput.getText().toString().trim();
